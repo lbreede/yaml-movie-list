a5bd24d049e5e0cb02c8538dd25de10498d02e7b
lbreede <lennart.breede@gmail.com>
2022-01-13 14:30:13 -0800

    added movies and tests

--- popular.py
+++ popular.py
@@ -1,30 +1,47 @@
 from tmdbv3api import TMDb, Movie
-import os.path
-# import os
-import json
-import yaml
 from auth import *
+import os.path
+import yaml
 
 tmdb = TMDb()
 tmdb.api_key = API_KEY
-tmdb.language = 'id'
 
-movie = Movie()
 
-popular = movie.popular()
 
-for m in popular:
+def yaml_dump_movies(lst):
+	
+	for m in lst:
 
-	year = m.release_date.split("-")[0]
-	title = m.title.lower().replace(" - ", "-").replace(" ", "-").replace(":", "")
-	path = f"movies/{year}"
+		year = m.release_date.split("-")[0]
+		title = m.title.lower().replace(" - ", "-").replace(" ", "-").replace(":", "")
+		path = f"movies/{year}"
 
-	if not os.path.isdir(path):
-		os.mkdir(path)
+		if not os.path.isdir(path):
+			os.mkdir(path)
 
-	path += f"/{title}.yaml"
+		path += f"/{title}.yaml"
 
-	obj = dict(m)
+		obj = dict(m)
 
-	with open(path, "w") as f:
-		yaml.dump(obj, f)
+		# force media type key
+		if "media_type" not in obj.keys():
+			obj["media_type"] = ""
+
+		# Force cast vote_average to float e.g. 6 -> 6.0
+		# obj["popularity"] = float(obj["popularity"]) # test not yet failed
+		obj["vote_average"] = float(obj["vote_average"])
+
+		with open(path, "w") as f:
+			yaml.dump(obj, f)
+
+def main():
+	movie = Movie()
+	movies = movie.popular()
+	# s = movie.search("The Lighthouse 1")
+	# first_result = s[0]
+	# movies = movie.recommendations(first_result.id)
+
+	yaml_dump_movies(movies)
+
+if __name__ == '__main__':
+	main()

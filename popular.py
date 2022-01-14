b1dc47f14a79e63d898d67b496525493031de074
lbreede <lennart.breede@gmail.com>
2022-01-13 17:33:30 -0800

    added a bunch of movies

--- popular.py
+++ popular.py
@@ -61,8 +61,8 @@ tmdb.api_key = API_KEY
 movie = Movie()
 
 def main():	
-	# save_popular_movies()
-	# save_movie_based_on_title_recommendations("Modern Times")
+	save_popular_movies()
+	save_movie_based_on_title_recommendations("Modern Times")
 	save_movie("The Northman")
 	save_movie("The Lighthouse")
 	save_movie("The VVitch")

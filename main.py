866f64de96fd4c10d123f887e2f3aabbc2a0a9fc
lbreede <lennart.breede@gmail.com>
2022-02-14 09:22:55 -0800

    black formatter

--- main.py
+++ main.py
@@ -4,65 +4,78 @@ import os.path
 import yaml
 import datetime
 
-def yaml_dump_movies(lst):	
-	for m in lst:
-		year, month, day = list(map(int, m.release_date.split("-")))
-		title = m.title.lower().replace(" - ", "-").replace(" ", "-").replace(":", "").replace("'", "")
-		path = f"movies/{year}"
 
-		if not os.path.isdir(path):
-			os.mkdir(path)
+def yaml_dump_movies(lst):
+    for m in lst:
+        year, month, day = list(map(int, m.release_date.split("-")))
+        title = (
+            m.title.lower()
+            .replace(" - ", "-")
+            .replace(" ", "-")
+            .replace(":", "")
+            .replace("'", "")
+        )
+        path = f"movies/{year}"
 
-		path += f"/{title}.yaml"
+        if not os.path.isdir(path):
+            os.mkdir(path)
 
-		obj = dict(m)
+        path += f"/{title}.yaml"
 
-		# Force media type key
-		if "media_type" not in obj.keys():
-			obj["media_type"] = ""
+        obj = dict(m)
 
-		# Force cast vote_average to float e.g. 6 -> 6.0
-		# obj["popularity"] = float(obj["popularity"]) # test not yet failed
-		obj["vote_average"] = float(obj["vote_average"])
+        # Force media type key
+        if "media_type" not in obj.keys():
+            obj["media_type"] = ""
 
-		dt_release_date = datetime.date(year, month, day)
+        # Force cast vote_average to float e.g. 6 -> 6.0
+        # obj["popularity"] = float(obj["popularity"]) # test not yet failed
+        obj["vote_average"] = float(obj["vote_average"])
 
-		# Add more custom keys
-		obj["release_date"] = dt_release_date
-		obj["year"] = year
-		obj["upcoming"] = False
-		
-		dt_today = datetime.date.today()
-		if dt_release_date > dt_today:
-			obj["upcoming"] = True
+        dt_release_date = datetime.date(year, month, day)
 
-		with open(path, "w") as f:
-			yaml.dump(obj, f)
+        # Add more custom keys
+        obj["release_date"] = dt_release_date
+        obj["year"] = year
+        obj["upcoming"] = False
+
+        dt_today = datetime.date.today()
+        if dt_release_date > dt_today:
+            obj["upcoming"] = True
+
+        with open(path, "w") as f:
+            yaml.dump(obj, f)
+
 
 def save_popular_movies():
-	popular = movie.popular()
-	yaml_dump_movies(popular)
+    popular = movie.popular()
+    yaml_dump_movies(popular)
+
 
 def save_movie_based_on_title_recommendations(title):
-	s = movie.search(title + " 1")[0]
-	recommendations = movie.recommendations(s.id)
-	yaml_dump_movies(recommendations)
+    s = movie.search(title + " 1")[0]
+    recommendations = movie.recommendations(s.id)
+    yaml_dump_movies(recommendations)
+
 
 def save_movie(title):
-	s = movie.search(title + " 1")[0]
-	yaml_dump_movies([s])
+    s = movie.search(title + " 1")[0]
+    yaml_dump_movies([s])
+
 
 tmdb = TMDb()
 tmdb.api_key = API_KEY
 movie = Movie()
 
-def main():	
-	save_popular_movies()
-	save_movie_based_on_title_recommendations("Star Wars")
-	save_movie("The Northman")
-	save_movie("The Lighthouse")
-	save_movie("The VVitch")
-	save_movie("Dune: Part Two")
 
-if __name__ == '__main__':
-	main()
+def main():
+    save_popular_movies()
+    save_movie_based_on_title_recommendations("Star Wars")
+    save_movie("The Northman")
+    save_movie("The Lighthouse")
+    save_movie("The VVitch")
+    save_movie("Dune: Part Two")
+
+
+if __name__ == "__main__":
+    main()
